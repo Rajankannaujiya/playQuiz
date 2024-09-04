74e0e920e63bc5c2b3d041f4a402f590e6125cce
enthusiast coder <139620395+Rajankannaujiya@users.noreply.github.com>
2024-09-04 15:22:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,9 @@ import cors from 'cors'
 
 const app =express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://play-quiz-ojnfrontend.vercel.app"
+    ));
 
 const PORT = process.env.PORT || 5000
 
